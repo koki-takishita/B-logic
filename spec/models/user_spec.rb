f4b11d287069e2423b8d25f98bd7e670eba9154f
koki-takishita <tkkokitk@yahoo.co.jp>
2021-05-08 14:58:51 +0900

    モデルスペック環境構築

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'メールアドレス、パスワードがあれば有効な状態であること'
+  it 'メールアドレスがなければ無効な状態であること'
+  it 'パスワードがなければ無効な状態であること'
+  it '重複したメールアドレスなら無効な状態であること'
 end
