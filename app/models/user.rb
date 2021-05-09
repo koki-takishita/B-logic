f83e21387aa7a3667529be57d8287d41c7a0636b
koki-takishita <tkkokitk@yahoo.co.jp>
2021-05-09 18:55:19 +0900

    バリテーションテスト作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
   # create,update時のみバリテーションを走らせる
-  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :email, uniqueness: true
+  # create時のみ走らせる
+  validates :password, presence: true, on: :create
+  validates :email, uniqueness: true, presence: true
 end
