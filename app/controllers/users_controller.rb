29cc2b9f2382583ce6e220e375dac02615ae9735
koki-takishita <tkkokitk@yahoo.co.jp>
2021-05-10 01:45:16 +0900

    ユーザーログイン機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
+  skip_before_action :require_login, only: [:index, :new, :create]
 
   # GET /users or /users.json
   def index
@@ -23,7 +24,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user, notice: "User was successfully created."
+      redirect_to users_path, notice: "User was successfully created."
     else
       render :new, status: :unprocessable_entity
     end
