1047244765daa66cce6f0da9042cac4e14e95b09
koki-takishita <tkkokitk@yahoo.co.jp>
2021-05-10 15:00:13 +0900

    ログイン機能テスト実装

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -57,7 +57,7 @@ RSpec.describe "Users", type: :system do
           fill_in 'Password', with: shortage_password_user.password
           fill_in 'Password confirmation', with: shortage_password_user.password_confirmation
           click_button 'Create User'
-          expect(page).to have_content "Password is too short (minimum is 6 characters)"
+          expect(page).to have_content 'Password is too short (minimum is 6 characters)'
         end
       end
       context 'パスワードと確認用パスワードが一致しない場合' do
@@ -77,8 +77,145 @@ RSpec.describe "Users", type: :system do
           fill_in 'Password', with: second_user.password
           fill_in 'Password confirmation', with: second_user.password_confirmation
           click_button 'Create User'
-          expect(page).to have_content "Email has already been taken"
+          expect(page).to have_content 'Email has already been taken'
         end
+      end
+    end
+  end
+
+  describe 'ログイン機能' do
+    let(:user) { create(:user) }
+
+    before do
+      visit login_path
+    end
+
+    context '登録済みのユーザー' do
+      it 'ログインできる' do
+        fill_in 'Email', with: user.email
+        # createだと、passwordが「""」になるため
+        # 直接'password'を入力している
+        fill_in 'Password', with: 'password'
+        click_button 'Login'
+        expect(page).to have_content 'Login successful'
+        expect(current_path).to eq users_path
+      end
+      context '一部情報を入力していない' do
+        context 'emailを入力していない' do
+          it 'ログインできない' do
+            fill_in 'Email', with: ''
+            fill_in 'Password', with: 'password'
+            click_button 'Login'
+            expect(page).to have_content 'Login failed'
+          end
+        end
+        context 'passwordを入力していない' do
+          it 'ログインできない' do
+            fill_in 'Email', with: user.email
+            fill_in 'Password', with: ''
+            click_button 'Login'
+            expect(page).to have_content 'Login failed'
+          end
+        end
+      end
+      context '属性が間違っている' do
+        context 'emailが間違っている' do
+          it 'ログインできない' do
+            fill_in 'Email', with: 'test@example.com'
+            fill_in 'Password', with: 'password'
+            click_button 'Login'
+            expect(page).to have_content 'Login failed'
+          end
+        end
+        context 'passwordが間違っている' do
+          it 'ログインできない' do
+            fill_in 'Email', with: user.email
+            fill_in 'Password', with: 'foobar'
+            click_button 'Login'
+            expect(page).to have_content 'Login failed'
+          end
+        end
+      end
+    end
+  end
+
+  describe 'ログアウト機能' do
+    let(:user) { create(:user) }
+    context 'ログイン済み' do
+
+      before do
+        login_as user
+      end
+
+      context 'indexページ' do
+        it 'ログアウトできる' do
+          visit users_path
+          click_link 'Logout'
+          expect(page).to have_content 'Logged out!'
+          expect(current_path).to eq users_path
+        end
+      end
+      context 'editページ' do
+        it 'ログアウトできる' do
+          visit edit_user_path(user)
+          click_link 'Logout'
+          expect(page).to have_content 'Logged out!'
+          expect(current_path).to eq users_path
+        end
+      end
+      context 'showページ' do
+        it 'ログアウトできる' do
+          visit user_path(user)
+          click_link 'Logout'
+          expect(page).to have_content 'Logged out!'
+          expect(current_path).to eq users_path
+        end
+      end
+    end
+  end
+
+  describe 'ユーザー編集機能' do
+    let(:user) { create(:user) }
+
+    before do
+      login_as user
+    end
+
+    context '属性一部変更' do
+
+      before do
+        visit edit_user_path(user)
+      end
+
+      context 'email変更' do
+        it '更新成功' do
+          fill_in 'Email', with: 'foobar@example.com'
+          click_button 'Update User'
+          expect(page).to have_content 'User was successfully updated.'
+          expect(current_path).to eq user_path(user)
+        end
+      end
+      context 'password変更' do
+        it '更新成功' do
+          fill_in 'Password', with: 'foobar'
+          fill_in 'Password confirmation', with: 'foobar'
+          click_button 'Update User'
+          expect(page).to have_content 'User was successfully updated.'
+          expect(current_path).to eq user_path(user)
+        end
+      end
+    end
+  end
+
+  describe 'ユーザー削除機能' do
+    context 'adminユーザー' do
+      it '削除できる' do
+        skip '未実装'
+      end
+    end
+    context '一般ユーザー' do
+      it '削除できない' do
+        skip '未実装'
       end
     end
   end
