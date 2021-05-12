0a0e2b3027eb177a4996f99555a74b304075cf51
koki-takishita <tkkokitk@yahoo.co.jp>
2021-05-12 20:35:29 +0900

    日本語化views

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,6 @@ module TentativeTitlePdca
     config.generators.system_tests = nil
     config.i18n.default_locale = :ja
     config.time_zone = 'Asia/Tokyo'
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
