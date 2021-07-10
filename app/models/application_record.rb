6b45168962b09e49edfed4998822bf7749c29ea5
koki-takishita <tkkokitk@yahoo.co.jp>
2021-07-10 23:50:57 +0900

    finish

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,19 +2,32 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def when_deadline(month)
+    byebug
     if self.persisted?
-      # 現在の締切日から❍ヶ月後
-      self.deadline_on = self.deadline_on.since(month.month)
+      # 締切日から❍ヶ月後
+      self.deadline_on = months_later(month: month, time: deadline_on)
       # ここに余りがあれば足す処理追加
       if self.respond_to?(:division_remainder)
-        self.deadline_on = self.deadline_on.since((self.division_remainder.to_i).days)
+        #self.deadline_on = self.deadline_on.since((self.division_remainder.to_i).days)
+        self.deadline_on = days_later(day: division_remainder, time: deadline_on)
       end
-      self.deadline_on
+      deadline_on
     else
       # 締切を今日の日付 + ❍ヶ月後に設定
-      deadline = Date.today >> month
-      self.deadline_on = deadline.end_of_day
+      self.deadline_on = months_later(month: month)
      end
+  end
+
+  def months_later(month: 0, time: current_time)
+    time.advance(months: month.to_i)
+  end
+
+  def days_later(day: 0, time: current_time)
+    time.advance(days: day.to_i)
+  end
+
+  def current_time
+    Time.zone.now
   end
 
 end
