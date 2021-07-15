47dd6a739c2e102bb206b1a5c3407725d8cd1bf5
koki-takishita <tkkokitk@yahoo.co.jp>
2021-07-16 03:04:50 +0900

    statusカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_15_175026) do
+ActiveRecord::Schema.define(version: 2021_07_15_175942) do
 
   create_table "goals", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "embodiment"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2021_07_15_175026) do
     t.datetime "deadline_on"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
