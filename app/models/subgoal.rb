991eacce9b98c942c68c5cc22f02827c02372369
koki-takishita <tkkokitk@yahoo.co.jp>
2021-07-17 04:04:16 +0900

    関連付けバグ修正

--- app/models/subgoal.rb
+++ app/models/subgoal.rb
@@ -2,6 +2,6 @@ class Subgoal < ApplicationRecord
   include Validators
   include Enum_status
   belongs_to :goal, dependent: :destroy
-  has_many :task
+  has_many :tasks
   validates :subgoal, presence: true, length: { maximum: 100 }
 end
