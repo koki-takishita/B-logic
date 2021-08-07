32d78f438b722f9e5d53bde2841641a7d2698c31
koki-takishita <tkkokitk@yahoo.co.jp>
2021-08-07 15:46:03 +0900

    ログインテスト共通化

--- spec/support/login_support.rb
+++ spec/support/login_support.rb
@@ -1,9 +1,12 @@
 module LoginSupport
   def login_as(user)
     visit root_path
-    fill_in 'user_session_email', with: user.email
-    fill_in 'user_session_password', with: 'password'
-    click_button 'login'
-    expect(page).to have_current_path root_path
+    click_button 'ログイン'
+    within('#LoginModal') do
+      fill_in 'session_email', with: user.email
+      fill_in 'session_password', with: 'password'
+      click_button 'ログイン'
+    end
+    expect(page).to have_content I18n.t 'user_sessions.create.success'
   end
 end
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :system do
+RSpec.describe "Users", type: :system, js: true do
   describe 'ユーザー新規作成機能' do
     let(:user) { build(:user) }
     let(:not_match_password_user) { build(:user, :not_match_password) }
@@ -8,43 +8,47 @@ RSpec.describe "Users", type: :system do
 
     before do
       visit root_path
+      click_button '新規登録'
     end
 
     context 'すべての属性が有効な状態' do
-      xit 'ユーザー新規作成に成功する' do
-        fill_in 'メールアドレス', with: user.email
-        fill_in 'パスワード', with: user.password
-        fill_in 'パスワード(確認)', with: user.password_confirmation
-        click_button '登録する'
-        expect(page).to have_content 'User was successfully created.'
+      it 'ユーザー新規作成に成功する' do
+        fill_in 'user_email', with: user.email
+        fill_in 'user_password', with: user.password
+        fill_in 'user_password_confirmation', with: user.password_confirmation
+        click_button I18n.t 'helpers.submit.create' 
+        expect(page).to have_content I18n.t 'users.create.success'
       end
     end
 
     context '一部属性を入力していない状態' do
       context 'emailを入力していない' do
-        xit 'ユーザー新規作成に失敗する' do
-          fill_in 'メールアドレス', with: nil
-          fill_in 'パスワード', with: user.password
-          fill_in 'パスワード(確認)', with: user.password_confirmation
-          click_button '登録する'
+        it 'ユーザー新規作成に失敗する' do
+          fill_in 'user_email', with: nil
+          fill_in 'user_password', with: user.password
+          fill_in 'user_password_confirmation', with: user.password_confirmation
+          click_button I18n.t 'helpers.submit.create' 
+          expect(page).to have_content  I18n.t 'users.create.danger'
           expect(page).to have_content 'メールアドレスを入力してください'
         end
       end
       context 'passwordを入力していない' do
-        xit 'ユーザー新規作成に失敗する' do
-          fill_in 'メールアドレス', with: user.email
-          fill_in 'パスワード', with: nil
-          fill_in 'パスワード(確認)', with: user.password_confirmation
-          click_button '登録する'
+        it 'ユーザー新規作成に失敗する' do
+          fill_in 'user_email', with: user.email
+          fill_in 'user_password', with: nil
+          fill_in 'user_password_confirmation', with: user.password_confirmation
+          click_button I18n.t 'helpers.submit.create' 
+          expect(page).to have_content  I18n.t 'users.create.danger'
           expect(page).to have_content 'パスワードを入力してください'
         end
       end
       context '確認用パスワードを入力していない' do
-        xit 'ユーザー新規作成に失敗する' do
-          fill_in 'メールアドレス', with: user.password
-          fill_in 'パスワード', with: user.password
-          fill_in 'パスワード(確認)', with: nil
-          click_button '登録する'
+        it 'ユーザー新規作成に失敗する' do
+          fill_in 'user_email', with: user.email
+          fill_in 'user_password', with: user.password
+          fill_in 'user_password_confirmation', with: nil
+          click_button I18n.t 'helpers.submit.create' 
+          expect(page).to have_content  I18n.t 'users.create.danger'
           expect(page).to have_content 'パスワード(確認)を入力してください'
         end
       end
@@ -52,31 +56,34 @@ RSpec.describe "Users", type: :system do
 
     context 'セキュリティー関連' do
       context 'パスワードが６文字未満の場合' do
-        xit 'ユーザー新規作成に失敗する' do
-          fill_in 'メールアドレス', with: shortage_password_user.email
-          fill_in 'パスワード', with: shortage_password_user.password
-          fill_in 'パスワード(確認)', with: shortage_password_user.password_confirmation
-          click_button '登録する'
+        it 'ユーザー新規作成に失敗する' do
+          fill_in 'user_email', with: shortage_password_user.email
+          fill_in 'user_password', with: shortage_password_user.password
+          fill_in 'user_password_confirmation', with: shortage_password_user.password_confirmation
+          click_button I18n.t 'helpers.submit.create' 
+          expect(page).to have_content  I18n.t 'users.create.danger'
           expect(page).to have_content 'パスワードは6文字以上で入力してください'
         end
       end
       context 'パスワードと確認用パスワードが一致しない場合' do
-        xit 'ユーザー新規作成に失敗する' do
-          fill_in 'メールアドレス', with: not_match_password_user.email
-          fill_in 'パスワード', with: not_match_password_user.password
-          fill_in 'パスワード(確認)', with: not_match_password_user.password_confirmation
-          click_button '登録する'
+        it 'ユーザー新規作成に失敗する' do
+          fill_in 'user_email', with: not_match_password_user.email
+          fill_in 'user_password', with: not_match_password_user.password
+          fill_in 'user_password_confirmation', with: not_match_password_user.password_confirmation
+          click_button I18n.t 'helpers.submit.create' 
+          expect(page).to have_content  I18n.t 'users.create.danger'
           expect(page).to have_content 'パスワード(確認)とパスワードの入力が一致しません'
         end
       end
       context '重複したメールアドレスの場合' do
-        xit 'ユーザー新規作成に失敗する' do
+        it 'ユーザー新規作成に失敗する' do
           first_user = create(:user, email:  'test@example.com')
           second_user = build(:user, email: 'test@example.com')
-          fill_in 'メールアドレス', with: second_user.email
-          fill_in 'パスワード', with: second_user.password
-          fill_in 'パスワード(確認)', with: second_user.password_confirmation
-          click_button '登録する'
+          fill_in 'user_email', with: second_user.email
+          fill_in 'user_password', with: second_user.password
+          fill_in 'user_password_confirmation', with: second_user.password_confirmation
+          click_button I18n.t 'helpers.submit.create' 
+          expect(page).to have_content  I18n.t 'users.create.danger'
           expect(page).to have_content 'メールアドレスはすでに存在します'
         end
       end
@@ -87,52 +94,62 @@ RSpec.describe "Users", type: :system do
     let(:user) { create(:user) }
 
     before do
-      visit login_path
+      visit root_path
+      click_button 'ログイン'
     end
 
     context '登録済みのユーザー' do
-      xit 'ログインできる' do
-        fill_in 'メールアドレス', with: user.email
-        # createだと、passwordが「""」になるため
-        # 直接'password'を入力している
-        fill_in 'パスワード', with: 'password'
-        click_button 'Login'
-        expect(page).to have_content 'Login successful'
-        expect(current_path).to eq users_path
+      it 'ログインできる' do
+        within('#LoginModal') do
+          fill_in 'session_email', with: user.email
+          # createだと、passwordが「""」になるため
+          # 直接'password'を入力している
+          fill_in 'session_password', with: 'password'
+          click_button 'ログイン'
+        end
+        expect(page).to have_content I18n.t 'user_sessions.create.success'
       end
       context '一部情報を入力していない' do
         context 'メールアドレスを入力していない' do
-          xit 'ログインできない' do
-            fill_in 'メールアドレス', with: ''
-            fill_in 'パスワード', with: 'password'
-            click_button 'Login'
-            expect(page).to have_content 'Login failed'
+          it 'ログインできない' do
+            within('#LoginModal') do
+              fill_in 'session_email', with: nil 
+              fill_in 'session_password', with: 'password'
+              click_button 'ログイン'
+            end
+            expect(page).to have_content I18n.t 'user_sessions.create.danger'
           end
         end
         context 'passwordを入力していない' do
-          xit 'ログインできない' do
-            fill_in 'メールアドレス', with: user.email
-            fill_in 'パスワード', with: ''
-            click_button 'Login'
-            expect(page).to have_content 'Login failed'
+          it 'ログインできない' do
+            within('#LoginModal') do
+              fill_in 'session_email', with: user.email
+              fill_in 'session_password', with: nil 
+              click_button 'ログイン'
+            end
+            expect(page).to have_content I18n.t 'user_sessions.create.danger'
           end
         end
       end
       context '属性が間違っている' do
         context 'メールアドレスが間違っている' do
-          xit 'ログインできない' do
-            fill_in 'メールアドレス', with: 'test@example.com'
-            fill_in 'パスワード', with: 'password'
-            click_button 'Login'
-            expect(page).to have_content 'Login failed'
+          it 'ログインできない' do
+            within('#LoginModal') do
+              fill_in 'session_email', with: 'test@example.com'
+              fill_in 'session_password', with: 'password'
+              click_button 'ログイン'
+            end
+            expect(page).to have_content I18n.t 'user_sessions.create.danger'
           end
         end
         context 'passwordが間違っている' do
-          xit 'ログインできない' do
-            fill_in 'メールアドレス', with: user.email
-            fill_in 'パスワード', with: 'foobar'
-            click_button 'Login'
-            expect(page).to have_content 'Login failed'
+          it 'ログインできない' do
+            within('#LoginModal') do
+              fill_in 'session_email', with: user.email 
+              fill_in 'session_password', with: 'foobar'
+              click_button 'ログイン'
+            end
+            expect(page).to have_content I18n.t 'user_sessions.create.danger'
           end
         end
       end
@@ -147,75 +164,11 @@ RSpec.describe "Users", type: :system do
         login_as user
       end
 
-      context 'indexページ' do
-        xit 'ログアウトできる' do
-          visit users_path
-          click_link 'Logout'
-          expect(page).to have_content 'Logged out!'
-          expect(current_path).to eq login_path
+      context 'topページ' do
+        it 'ログアウトできる' do
+          click_link 'ログアウト'
+          expect(page).to have_content I18n.t 'user_sessions.destroy.success'
         end
-      end
-      context 'editページ' do
-        xit 'ログアウトできる' do
-          visit edit_user_path(user)
-          click_link 'Logout'
-          expect(page).to have_content 'Logged out!'
-          expect(current_path).to eq login_path
-        end
-      end
-      context 'showページ' do
-        xit 'ログアウトできる' do
-          visit user_path(user)
-          click_link 'Logout'
-          expect(page).to have_content 'Logged out!'
-          expect(current_path).to eq login_path
-        end
-      end
-    end
-  end
-
-  describe 'ユーザー編集機能' do
-    let(:user) { create(:user) }
-
-    before do
-      login_as user
-    end
-
-    context '属性一部変更' do
-
-      before do
-        visit edit_user_path(user)
-      end
-
-      context 'メールアドレス変更' do
-        xit '更新成功' do
-          fill_in 'メールアドレス', with: 'foobar@example.com'
-          click_button '更新する'
-          expect(page).to have_content 'User was successfully updated.'
-          expect(current_path).to eq user_path(user)
-        end
-      end
-      context 'password変更' do
-        xit '更新成功' do
-          fill_in 'パスワード', with: 'foobar'
-          fill_in 'パスワード(確認)', with: 'foobar'
-          click_button '更新する'
-          expect(page).to have_content 'User was successfully updated.'
-          expect(current_path).to eq user_path(user)
-        end
-      end
-    end
-  end
-
-  describe 'ユーザー削除機能' do
-    context 'adminユーザー' do
-      xit '削除できる' do
-        skip '未実装'
-      end
-    end
-    context '一般ユーザー' do
-      xit '削除できない' do
-        skip '未実装'
       end
     end
   end
