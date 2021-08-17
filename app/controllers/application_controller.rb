f992fc69bf50ef2949421d8790ea54fb083834ba
koki-takishita <tkkokitk@yahoo.co.jp>
2021-08-17 23:56:28 +0900

    エラー機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,7 +28,37 @@ class ApplicationController < ActionController::Base
   private
 
     def read_expired_models(models)
-      models.map{|model| model.where(status: 2, user_id: current_user.id) }
+      models.map{|model|
+        instances = model.where(user_id: current_user.id).where.not(status: 1)
+        model_time_check(instances)
+      }
+    end
+
+    # 時間を比較して、今の時間より遅かったらexpired!
+    def model_time_check(instances)
+      instances.map{|instance|
+        case instance
+        when Goal then
+          instance if instance.expired! if instance.deadline <= Time.zone.now
+        when Task then
+          instance if instance.expired! if time_conmparison(instance)
+        end
+      }
+    end
+
+    def time_conmparison(instance)
+      target = convert_date_to_time(instance.reminder.hour, instance.reminder.min)
+      target < current_time
+    end
+
+    def convert_date_to_time(hour, min)
+      Tod::TimeOfDay.new hour, min
+    end
+
+    def current_time
+      hour = Time.zone.now.hour
+      min = Time.zone.now.min
+      convert_date_to_time(hour, min)
     end
 
     def models_hash
@@ -37,7 +67,7 @@ class ApplicationController < ActionController::Base
 
     def push_records(model1, model2)
       records = []
-      records << model1
-      records << model2
+      records << model1 unless model1 == nil
+      records << model2 unless model2 == nil
     end
 end
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -4,7 +4,7 @@ class Goal < ApplicationRecord
   belongs_to :user
   has_many :issues, dependent: :destroy
   validates :name, :deadline, presence: true
-  validate :deadline_cannot_be_in_the_past 
+  validate :deadline_cannot_be_in_the_past, on: :create
 
   def deadline_cannot_be_in_the_past
     if deadline.present? && deadline <= Date.today
