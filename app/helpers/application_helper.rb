a5ba259364fe1e3ef60599f5bf0a8da596c02843
koki-takishita <tkkokitk@yahoo.co.jp>
2021-08-29 04:40:58 +0900

    見た目調整完了

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,4 @@
 module ApplicationHelper
-  def order_join(*names)
-    object = names.map{|name| name.to_s}
-    return object.join
-  end
-
-  def selected_goal
-    @selected_goal ||= current_user.goals.find_by_id(session[:goal_id]) if session[:goal_id]
-  end
-
-  def selected_subgoal
-    @selected_subgoal ||= selected_goal.subgoals.find_by_id(session[:subgoal_id]) if session[:subgoal_id]
-  end
-
-  def selected_task
-    @selected_task ||= selected_subgoals.find_by_id(session[:task_id]) if session[:task_id]
-  end
 
   def active_if(action: nil, id: nil)
     return active_instance(id) if id && model_instance
@@ -22,7 +6,16 @@ module ApplicationHelper
   end
 
   def model_instance
-    @issue || @goal
+    @goal || issue_instance || task_instance
+  end
+
+  def issue_instance
+    return @issue.goal if action_name == 'create' && controller_name == 'issues'
+    @issue
+  end
+
+  def task_instance
+    @task.issue if @task || nil
   end
 
   def active_instance(id)
